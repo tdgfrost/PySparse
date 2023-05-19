ac237c544c7a5ee4991cf035af1ac5ceb84a2883
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2023-05-19 15:41:50 +0100

    Reversed to_sparse njit changes
    Reversed some of the njit changes made to to_sparse, as I realised they weren't compatible with array formats like h5py.

--- sparse/core.py
+++ sparse/core.py
@@ -1,10 +1,8 @@
 import numpy as np
 from numpy.lib.format import open_memmap
-from tqdm import tqdm
 import os
-from numba import njit, types, typed, prange
+from numba import njit, prange
 import pickle
-from math import ceil
 
 """
 ============================================================
@@ -24,8 +22,26 @@ def announce_progress(message: str) -> None:
     return
 
 
-@njit(parallel=True)
-def __calc_sparse_shape(array: np.ndarray) -> tuple:
+def progress_bar(iteration, total_iterations):
+    """
+    Print a progress bar to the console
+    :param iteration: current iteration
+    :param total_iterations: total number of iterations
+    :return: None
+    """
+    bar_length = 30
+
+    percent = iteration / total_iterations
+    percent_complete = int(percent * 100)
+
+    progress = int(percent * bar_length)
+    progress = '[' + '#' * progress + ' ' * (bar_length - progress) + ']'
+
+    print(f'\r{progress} {percent_complete}% complete', end='', flush=True)
+    return
+
+
+def __calc_sparse_shape(array: np.ndarray, chunksize: int, verbose: bool) -> tuple:
     """
     Calculate the shape of the (pending) sparse array
     :param array: dense numpy array
@@ -33,11 +49,17 @@ def __calc_sparse_shape(array: np.ndarray) -> tuple:
     :param verbose: whether to print progress statements
     :return: tuple of shape
     """
+
     data_shape = 0
     shape = array.shape
 
-    for i in prange(shape[0]):
-        data_shape += np.count_nonzero(array[i])
+    if (chunksize is None) or (type(array) is np.ndarray):
+        data_shape = np.count_nonzero(array)
+
+    else:
+        for i in range(0, shape[0], chunksize):
+            progress_bar(i, shape[0]) if verbose else None
+            data_shape += np.count_nonzero(array[i:i + chunksize])
 
     return (data_shape,)
 
@@ -45,13 +67,15 @@ def __calc_sparse_shape(array: np.ndarray) -> tuple:
 def __convert_to_sparse_data(array_chunk: np.ndarray, iteration: int) -> (np.ndarray, np.ndarray):
     """
     Convert a chunk of a dense array to sparse data
-    :param array_chunk: chunk of dense array
-    :param iteration: chunk index
+    :param array: chunk of dense array
+    :param dense_size: size of dense array
+    :param chunksize: number of rows of array to process at a time
+    :param memmap_sparse_coords: memory-mapped array to store sparse coordinates
+    :param memmap_sparse_data: memory-mapped array to store sparse data
     :return: tuple of sparse coordinates and sparse values
     """
-
     sparse_coords = np.nonzero(array_chunk)
-    sparse_values = array_chunk.flatten()[np.flatnonzero(array_chunk)]
+    sparse_values = array_chunk[sparse_coords]
     sparse_coords = list(sparse_coords)
     sparse_coords[0] += iteration
     sparse_coords = np.stack(sparse_coords, axis=1)
@@ -94,18 +118,18 @@ def __write_sparse_arrays(array: np.ndarray or np.memmap, path: 'str', chunksize
     dense_shape = array.shape
     dense_dtype = array.dtype
     announce_progress('Identifying sparse shape...') if verbose else None
-    nonzero_shape = __calc_sparse_shape(array)
+    nonzero_shape = __calc_sparse_shape(array, chunksize, verbose)
 
     # Create the sparse array binaries (memory-mapped)
     memmap_sparse_data = open_memmap(os.path.join(path, 'sparse_data.npy'),
                                      dtype=dense_dtype,
                                      mode='w+',
-                                     shape=nonzero_shape)
+                                     shape=(nonzero_shape,))
 
     memmap_sparse_coords = open_memmap(os.path.join(path, 'sparse_coords.npy'),
                                        dtype=np.int32,
                                        mode='w+',
-                                       shape=(nonzero_shape[0], len(dense_shape)))
+                                       shape=(nonzero_shape, len(dense_shape)))
 
     np.save(os.path.join(path, 'dense_shape.npy'), dense_shape)
 
@@ -118,28 +142,26 @@ def __write_sparse_arrays(array: np.ndarray or np.memmap, path: 'str', chunksize
 
         memmap_sparse_coords[:] = sparse_coords
         memmap_sparse_data[:] = sparse_values
-        with open(os.path.join(path, 'sparse_coords_dict.pkl'), 'wb') as f:
-            pickle.dump(sparse_coords_dict, f)
 
     else:
         sparse_coords_dict = {}
-        for chunk_idx in tqdm(range(0, dense_shape[0], chunksize)) if verbose else range(0, dense_shape[0], chunksize):
-            sparse_coords, sparse_values, sparse_coords_dict_iter = __convert_to_sparse_data(
-                array[chunk_idx:chunk_idx + chunksize], chunk_idx)
+        for chunk_idx in range(0, dense_shape[0], chunksize):
+            progress_bar(chunk_idx, dense_shape[0]) if verbose else None
+            sparse_coords, sparse_values, sparse_coords_dict_temp = __convert_to_sparse_data(array[chunk_idx:chunk_idx + chunksize], chunk_idx)
 
             memmap_sparse_coords[sparse_index:sparse_index + sparse_coords.shape[0]] = sparse_coords
             memmap_sparse_data[sparse_index:sparse_index + sparse_coords.shape[0]] = sparse_values
+            sparse_coords_dict.update(sparse_coords_dict_temp)
 
             sparse_index += sparse_coords.shape[0]
-            sparse_coords_dict.update(sparse_coords_dict_iter)
 
-        with open(os.path.join(path, 'sparse_coords_dict.pkl'), 'wb') as f:
-            pickle.dump(sparse_coords_dict, f)
+    with open(os.path.join(path, 'sparse_coords_dict.pkl'), 'wb') as f:
+        pickle.dump(dict(sparse_coords_dict), f)
 
     return
 
 
-def to_sparse(array: np.ndarray or np.memmap, savepath: 'str', chunksize=1000, verbose=True) -> None:
+def to_sparse(array: np.ndarray or np.memmap, savepath: 'str', chunksize=100000, verbose=True) -> None:
     """
     Convert and write a dense array to a sparse array
     :param array: numpy array to be converted
